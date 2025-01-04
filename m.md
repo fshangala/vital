af5f671060612bd11690fbe5159285e7c008e018
fshangala <fshangala@gmail.com>
2025-01-04 10:15:01 +0200

    Add logo image to Service component and create initial m.md file

---
+++ m.md
@@ -0,0 +1,11 @@
+- chat on whatsapp
+- prepare quotation for the client and send them an invoice to pay
+- bank transfer
+- name: vital graphics and marketing agency
+- Review page
+- blog link facebook page
+
+# Need
+- Facebook link
+- Email
+- Logo
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,6 +2,8 @@ import { Card } from "primereact/card";
 
 export default function Service({title}) {
   return (
-    <Card title={title}></Card>
+    <Card start={(
+      <img src="images/logo.png" alt={$title} />
+    )} title={title}></Card>
   )
 }
